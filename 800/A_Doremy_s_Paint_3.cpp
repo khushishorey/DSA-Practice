60e8f1cf76a01c4db950e10513cae52f53efd854
khushi <khushishorey2005@gmail.com>
2026-01-22 15:36:20 +0530

    solved few problems

--- 800/A_Doremy_s_Paint_3.cpp
+++ 800/A_Doremy_s_Paint_3.cpp
@@ -11,6 +11,25 @@ int main() {
         for(int i = 0; i < n; i++) {
             cin>> v[i];
         }
+
+        map<int,int> freq;
+
+        for(int i = 0; i < n; i++) {
+            freq[v[i]]++;               // caclulating frequency of each element in vector
+        }
+
+        if(freq.size() >= 3) cout << "NO" << endl;
+
+        else {
+            int f1 = freq.begin()->second;
+            int f2 = freq.rbegin()->second;
+
+            if(f1 == f2) cout << "YES" <<endl;
+            else if(n % 2 != 0 && abs(f1-f2) == 1) cout << "YES" <<endl;
+            else cout << "NO" <<endl;
+
+        }
+
         
     }
 
--- 800/A_Two_Permutations.cpp
+++ 800/A_Two_Permutations.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 
 int main() {
+    int t;
+    cin >> t;
+    while(t--) {
+        int n,a,b;
+        cin >> n >> a >> b;
+        if((a+b+2 <= n) || (a == b && a == n)) cout << "Yes" <<endl;
+        else cout << "No"<< endl;
+    }
     return 0;
 }
-
